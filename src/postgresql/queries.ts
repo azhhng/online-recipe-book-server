0b694132f205d2efa3dbc8fb12218b75d8d6954c
azhhng <azhhng@gmail.com>
2023-06-15 15:53:35 -0400

    using serial to create unique id not sequence

--- src/postgresql/queries.ts
+++ src/postgresql/queries.ts
@@ -53,7 +53,7 @@ const createRecipeBox = async (
   color: string
 ) => {
   const response = await pool.query(
-    `INSERT INTO "recipe_box" (user_id, recipe_box_id, name, description, emoji, color) VALUES ($1, nextval('recipe_box_id_sequence'), $2, $3, $4, $5) RETURNING *`,
+    `INSERT INTO "recipe_box" (user_id, name, description, emoji, color) VALUES ($1, $2, $3, $4, $5) RETURNING *`,
     [userId, name, description, emoji, color]
   );
   return response.rows;
@@ -94,8 +94,8 @@ const createRecipe = async (
   recipe_box_id: BigInt
 ) => {
   const response = await pool.query(
-    `INSERT INTO "recipe" (recipe_id, user_id, name, link, description, has_made, favorite, recipe_box_id)
-    VALUES (nextval('recipe_id_sequence'), $1, $2, $3, $4, $5, $6, $7) RETURNING *`,
+    `INSERT INTO "recipe" (user_id, name, link, description, has_made, favorite, recipe_box_id)
+    VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *`,
     [userId, name, link, description, has_made, favorite, recipe_box_id]
   );
   return response.rows;
