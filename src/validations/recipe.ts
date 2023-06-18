e353201746a0789a26f012b4d5fead1cefbe1ffc
azhhng <azhhng@gmail.com>
2023-06-18 14:54:18 -0400

    add validation for recipe body

---
+++ src/validations/recipe.ts
@@ -0,0 +1,29 @@
+import { logger } from "../logger";
+const fileName = "recipe.ts";
+const validateRecipeBody = (
+  name: string,
+  link: string,
+  recipe_box_id: string
+) => {
+  try {
+    if (!name) {
+      throw new Error(`Cannot create a recipe without a valid name.`);
+    }
+    if (!link) {
+      throw new Error(`Cannot create a recipe without a valid link.`);
+    }
+    if (!recipe_box_id) {
+      throw new Error(`Cannot create a recipe without a recipe box ID.`);
+    }
+  } catch (error) {
+    logger(
+      fileName,
+      "validateRecipeBody",
+      `There was an error validating a recipe body.`,
+      error
+    );
+    throw error;
+  }
+};
+
+export { validateRecipeBody };
